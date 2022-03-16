a79a6ad54cce71999e17521f991b9ab1bd6539b7
Greg Bugaj <gregbugaj@yahoo.com>
2022-03-16 17:37:32 -0500

    Refactoring

--- src/boxes/TextFuseNetBoxProcessor.py
+++ src/boxes/TextFuseNetBoxProcessor.py
@@ -1,4 +1,5 @@
-class TextFuseNetBoxProcessor():
+
+class TextFuseNetBoxProcessor:
     """"TextFuseNet box processor responsible for extracting bounding boxes for given documents"""
     def __init__(self):
         pass
