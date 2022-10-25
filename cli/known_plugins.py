61bf7468e74c2d15acdfa688472f598a28bb0ab9
Greg <gregbugaj@yahoo.com>
2022-10-24 23:27:04 -0500

    Add black formatter

--- cli/known_plugins.py
+++ cli/known_plugins.py
@@ -1,6 +1,6 @@
 plugin_info = {
-    'now': {  # your sub-command
-        'display-name': 'Marie Now',  # the name of your plugin/project
-        'pip-package': 'marie-now',  # the pip package to install
+    "now": {  # your sub-command
+        "display-name": "Marie Now",  # the name of your plugin/project
+        "pip-package": "marie-now",  # the pip package to install
     }
 }
