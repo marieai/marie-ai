8e2f6bc21e35247f9b8d08099c061aa4396760e4
gbugaj <gbugaj@rmsweb.com>
2022-10-26 16:38:04 -0500

    Add category json

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/greg/dev/marie-ai/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/greg/dev/marie-ai/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/greg/dev/marie-ai/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/greg/dev/marie-ai/docs/src/css/custom.css'),
-];
