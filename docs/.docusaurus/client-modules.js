c3fc048e60e6f34c1b8db28cba9ff1daf9170b9c
Greg <gregbugaj@yahoo.com>
2022-10-26 03:24:55 -0500

    WIP: Update docs

--- docs/.docusaurus/client-modules.js
+++ docs/.docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require('/home/gbugaj/dev/marie-ai/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/gbugaj/dev/marie-ai/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/gbugaj/dev/marie-ai/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/gbugaj/dev/marie-ai/docs/src/css/custom.css'),
+  require('/home/greg/dev/marie-ai/docs/node_modules/infima/dist/css/default/default.css'),
+  require('/home/greg/dev/marie-ai/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/greg/dev/marie-ai/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/greg/dev/marie-ai/docs/src/css/custom.css'),
 ];
