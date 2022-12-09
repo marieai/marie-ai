953497b996b138b8cccedd9ad7c1d776c69442f8
gbugaj <gbugaj@rmsweb.com>
2022-12-09 17:16:10 -0600

    chore: add hello-marie example

---
+++ examples/hello-marie/README.md
@@ -0,0 +1,6 @@
+
+## Start flow
+
+```shell
+marie flow --uses ./flow.yml --no-crud-endpoints --no-debug-endpoints --log-config profile
+```
---
+++ examples/hello-marie/executor1/executor.py
@@ -0,0 +1,16 @@
+import numpy as np
+import torch
+
+from marie import Executor, requests, DocumentArray
+
+
+class MyExecutorAA(Executor):
+    @requests(on="/aa")
+    def foo(self, docs: DocumentArray, **kwargs):
+        print('AAA')
+        docs[0].text = 'AA - hello, world!'
+        docs[1].text = 'AA - goodbye, world!'
+
+    @requests(on='/crunch-numbers-aa')
+    def bar(self, docs: DocumentArray, **kwargs):
+        docs[0].text = 'crunch, aa'
