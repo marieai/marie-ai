d9f1cb06bc81adc66ecb969b133d0ccadb66a750
gbugaj <gbugaj@rmsweb.com>
2022-12-13 17:20:28 -0600

    feat(ci): Initial release scripts

---
+++ scripts/black.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+pip install black==22.3.0
+arrVar=()
+echo we ignore non-*.py files and files generated from protobuf
+excluded_files=(
+   marie/proto/pb/jina_pb2.py
+   marie/proto/pb/jina_pb2_grpc.py
+   marie/proto/pb2/jina_pb2.py
+   marie/proto/pb2/jina_pb2_grpc.py
+   docs/conf.py
+)
+for changed_file in $CHANGED_FILES; do
+  if [[ ${changed_file} == *.py ]] && ! [[ " ${excluded_files[@]} " =~ " ${changed_file} " ]]; then
+    echo checking ${changed_file}
+    arrVar+=(${changed_file})
+  fi
+done
+if (( ${#arrVar[@]} )); then
+  black -S --check "${arrVar[@]}"
+fi
+echo "no files left to check"
+exit 0
