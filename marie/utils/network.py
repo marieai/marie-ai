4be3118ca75eeea02b78aab3ac362430cadc530f
gregbugaj <gregbugaj@yahoo.com>
2022-12-27 17:13:35 -0600

    feat(discovery): consul discovery service, resolves #43

--- marie/utils/network.py
+++ marie/utils/network.py
@@ -27,14 +27,14 @@ def get_ip_address():
     https://stackoverflow.com/questions/24196932/how-can-i-get-the-ip-address-from-nic-in-python
     """
     # TODO : Add support for IP detection
-    ## if there is an access to external network we can try this
+    # if there is an access to external network we can try this
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
             s.connect(("8.8.8.8", 80))
             sockname = s.getsockname()
             return s.getsockname()[0]
     except Exception as e:
-        raise e  # For debug
+        # raise e  # For debug
         pass
 
     return "127.0.0.1"
