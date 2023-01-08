7d8732af3d6f6f88c89944a347aff517785e8889
Greg <gregbugaj@yahoo.com>
2023-01-08 00:56:04 -0600

    fix: disabled warnings, regen proto

--- marie/__init__.py
+++ marie/__init__.py
@@ -18,15 +18,24 @@ from distutils.util import strtobool as strtobool
 if _sys.version_info < (3, 8, 0):
     raise OSError(f"Marie requires Python >= 3.8, but yours is {_sys.version_info}")
 
-if strtobool(_os.environ.get("MARIE_SUPPRESS_WARNINGS", "true")):
+if True or strtobool(_os.environ.get("MARIE_SUPPRESS_WARNINGS", "true")):
     import warnings
 
-    warnings.simplefilter(action='ignore', category=FutureWarning)
-    warnings.simplefilter(action='ignore', category=UserWarning)
+    warnings.simplefilter(action="ignore", category=FutureWarning)
+    warnings.simplefilter(action="ignore", category=UserWarning)
+    warnings.simplefilter(action="ignore", category=DeprecationWarning)
+
+    # this will suppress all the warning
+    def warn(*args, **kwargs):
+        pass
+
+    import warnings
+
+    warnings.warn = warn
 
 
 def _warning_on_one_line(message, category, filename, lineno, *args, **kwargs):
-    return '\033[1;33m%s: %s\033[0m \033[1;30m(raised from %s:%s)\033[0m\n' % (
+    return "\033[1;33m%s: %s\033[0m \033[1;30m(raised from %s:%s)\033[0m\n" % (
         category.__name__,
         message,
         filename,
@@ -35,54 +44,54 @@ def _warning_on_one_line(message, category, filename, lineno, *args, **kwargs):
 
 
 _warnings.formatwarning = _warning_on_one_line
-_warnings.simplefilter('always', DeprecationWarning)
+_warnings.simplefilter("always", DeprecationWarning)
 # fix fork error on MacOS but seems no effect? must do EXPORT manually before jina start
-_os.environ['OBJC_DISABLE_INITIALIZE_FORK_SAFETY'] = 'YES'
+_os.environ["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
 
 # JINA_MP_START_METHOD has higher priority than os-patch
-_start_method = _os.environ.get('JINA_MP_START_METHOD', None)
-if _start_method and _start_method.lower() in {'fork', 'spawn', 'forkserver'}:
+_start_method = _os.environ.get("JINA_MP_START_METHOD", None)
+if _start_method and _start_method.lower() in {"fork", "spawn", "forkserver"}:
     from multiprocessing import set_start_method as _set_start_method
 
     try:
         _set_start_method(_start_method.lower())
         _warnings.warn(
-            f'multiprocessing start method is set to `{_start_method.lower()}`'
+            f"multiprocessing start method is set to `{_start_method.lower()}`"
         )
     except Exception as e:
         _warnings.warn(
-            f'failed to set multiprocessing start_method to `{_start_method.lower()}`: {e!r}'
+            f"failed to set multiprocessing start_method to `{_start_method.lower()}`: {e!r}"
         )
-elif _sys.version_info >= (3, 8, 0) and _platform.system() == 'Darwin':
+elif _sys.version_info >= (3, 8, 0) and _platform.system() == "Darwin":
     # DO SOME OS-WISE PATCHES
 
     # temporary fix for python 3.8 on macos where the default start is set to "spawn"
     # https://docs.python.org/3/library/multiprocessing.html#contexts-and-start-methods
     from multiprocessing import set_start_method as _set_start_method
 
-    _set_start_method('fork')
+    _set_start_method("fork")
 
 # do not change this line manually
 # this is managed by git tag and updated on every release
 # NOTE: this represents the NEXT release version
 
-__version__ = '3.0.0'
+__version__ = "3.0.0"
 
 # do not change this line manually
 # this is managed by proto/build-proto.sh and updated on every execution
-__proto_version__ = '0.1.13'
+__proto_version__ = "0.1.13"
 
 try:
     __docarray_version__ = _docarray.__version__
 except AttributeError as e:
     raise RuntimeError(
-        '`docarray` dependency is not installed correctly, please reinstall with `pip install -U --force-reinstall docarray`'
+        "`docarray` dependency is not installed correctly, please reinstall with `pip install -U --force-reinstall docarray`"
     )
 
 try:
     _signal.signal(_signal.SIGINT, _signal.default_int_handler)
 except Exception as exc:
-    _warnings.warn(f'failed to set default signal handler: {exc!r}`')
+    _warnings.warn(f"failed to set default signal handler: {exc!r}`")
 
 
 def _set_nofile(nofile_atleast=4096):
@@ -116,10 +125,10 @@ def _set_nofile(nofile_atleast=4096):
         except (ValueError, res.error):
             try:
                 hard = soft
-                print(f'trouble with max limit, retrying with soft,hard {soft},{hard}')
+                print(f"trouble with max limit, retrying with soft,hard {soft},{hard}")
                 res.setrlimit(res.RLIMIT_NOFILE, (soft, hard))
             except Exception:
-                print('failed to set ulimit, giving up')
+                print("failed to set ulimit, giving up")
                 soft, hard = res.getrlimit(res.RLIMIT_NOFILE)
 
     return soft, hard
