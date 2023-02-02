715312e48750cb68a79bb0e28d20b4bcc503b442
scstauf <scstauf@gmail.com>
2023-02-02 16:48:48 -0600

    Added WhitelistMiddleware for whitelisting ip addresses. Use the ip_whitelist_path parameter on HTTPGateway to specify a file containing CRLF delimited strings of CIDR compliant IP addresses.

--- marie/serve/runtimes/gateway/http/__init__.py
+++ marie/serve/runtimes/gateway/http/__init__.py
@@ -1,3 +1,4 @@
 from marie.serve.runtimes.gateway.http.gateway import HTTPGateway
+from marie.serve.runtimes.gateway.http.middleware.whitelist_middleware import WhitelistMiddleware
 
 __all__ = ['HTTPGateway']
