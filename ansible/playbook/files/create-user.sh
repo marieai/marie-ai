354009e10f1da9ee7797098a8581a67081ddd52a
Grzegorz Bugaj <gregbugaj@yahoo.com>
2023-03-29 18:36:27 -0500

    wip:automation

---
+++ ansible/playbook/files/create-user.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [[ -z $1 ]]; then
+    echo "No user specified"
+    exit 1
+fi
+
+if id -u $1 >/dev/null 2>&1; then
+    echo "User $1 already exists, skipping"
+    exit 0
+fi
+
+sudo groupadd -r app-svc -g 433
+sudo useradd -u 431 --comment 'app-svc' --create-home app-svc  --shell /bin/bash -g app-svc
+
+# add user to docker group
+sudo usermod -aG docker app-svc
+
+# sudo useradd -m -s /bin/bash -u 431 -g 433 app-svc
