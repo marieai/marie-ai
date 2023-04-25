d0f63df2a9bbfd3467779962d610eb6701b01647
Greg <gregbugaj@yahoo.com>
2023-04-25 06:19:42 -0500

    feat: initial working version of the plugin

--- cvat/serverless/pytorch/bounding-boxes/nuclio/main.py
+++ cvat/serverless/pytorch/bounding-boxes/nuclio/main.py
@@ -4,6 +4,8 @@ from PIL import Image
 import io
 from model_handler import ModelHandler
 
+# https://opencv.github.io/cvat/docs/manual/advanced/serverless-tutorial/
+
 
 def init_context(context):
     context.logger.info("Init context...  0%")
@@ -13,42 +15,50 @@ def init_context(context):
 
 
 def handler(context, event):
-    context.logger.info("marie-ai  handler")
+    context.logger.info("marie-ai  handler 001")
+
     data = event.body
     buf = io.BytesIO(base64.b64decode(data["image"]))
     image = Image.open(buf)
     image = image.convert("RGB")  #  to make sure image comes in RGB
     results_json = context.user_data.model.handle(image)
-    results_json = [
+
+    context.logger.info("marie-ai  handler 002")
+    results_json_test = [
         {
-            'confidence': 0.9,
-            'name': 'bbox',
-            'xmin': 0,
-            'ymin': 0,
-            'xmax': 100,
-            'ymax': 100,
+            "confidence": 0.9,
+            "name": "bbox",
+            "xmin": 0,
+            "ymin": 0,
+            "xmax": 100,
+            "ymax": 100,
         }
     ]
 
+    box_format = "xywh"
     encoded_results = []
     for result in results_json:
+        # convert int32 to int
+        box = [int(v) for v in result]
+
+        if box_format == "xywh":
+            box = [box[0], box[1], box[0] + box[2], box[1] + box[3]]
+
         encoded_results.append(
             {
-                'confidence': result['confidence'],
-                'label': result['name'],
-                'points': [
-                    result['xmin'],
-                    result['ymin'],
-                    result['xmax'],
-                    result['ymax'],
-                ],
-                'type': 'rectangle',
+                "confidence": str(1.0),
+                "label": "bbox",
+                "points": box,
+                "type": "rectangle",
             }
         )
 
+    results = json.dumps(encoded_results)
+    context.logger.info(results)
+
     return context.Response(
-        body=json.dumps(encoded_results),
+        body=results,
         headers={},
-        content_type='application/json',
+        content_type="application/json",
         status_code=200,
     )
--- cvat/serverless/pytorch/bounding-boxes/nuclio/model_handler.py
+++ cvat/serverless/pytorch/bounding-boxes/nuclio/model_handler.py
@@ -1,25 +1,28 @@
-import numpy as np
-import cv2
 import torch
+from PIL import Image
 
-
-def convert_mask_to_polygon(mask):
-    contours = []
-    polygon = []
-    for point in contours:
-        polygon.append([int(point[0]), int(point[1])])
-
-    return polygon
+from marie.boxes import BoxProcessorUlimDit, PSMode
+from marie.boxes.dit.ulim_dit_box_processor import visualize_bboxes
 
 
 class ModelHandler:
     def __init__(self):
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.checkpoint = "model.pth"
-        self.model_type = "vit_h"
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.latest_image = None
-        self.predictor = None
+        self.predictor = BoxProcessorUlimDit(
+            models_dir="/etc/marie/model_zoo/unilm/dit/text_detection",
+            cuda=torch.cuda.is_available(),
+        )
 
     def handle(self, image):
         self.latest_image = image
-        return None, None
+
+        (
+            boxes,
+            fragments,
+            lines,
+            _,
+            lines_bboxes,
+        ) = self.predictor.extract_bounding_boxes("cvat", "field", image, PSMode.SPARSE)
+
+        return boxes
