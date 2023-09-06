132a90c208c39f10080f8b7c48a1442f1375e60d
Grzegorz Bugaj <gregbugaj@yahoo.com>
2023-09-06 14:31:45 -0500

    wip:cleanup

--- ansible/play-start-node.sh
+++ ansible/play-start-node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ANSIBLE_VAULT_PASSWORD_FILE=./vault.txt
-ansible-playbook  ./playbook/start-node.yml -i ./inventories/hosts.yml -u gpu-svc --become --become-user gpu-svc \
+ansible-playbook -vvv  ./playbook/start-node.yml -i ./inventories/hosts.yml -u gpu-svc --become --become-user gpu-svc \
  -e '@password.yml'  --vault-password-file=vault.txt
 
 
