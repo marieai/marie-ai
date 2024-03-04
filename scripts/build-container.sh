ba6545250f1908d18977c7809680a58921e2444a
Greg Bugaj <gregbugaj@yahoo.com>
2024-03-04 16:20:10 +0000

    chore: version bump

--- scripts/build-container.sh
+++ scripts/build-container.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 set -e
-DOCKER_BUILDKIT=1 docker build . --cpuset-cpus="0-32"  --build-arg PIP_TAG="[standard]" -f ./Dockerfiles/gpu-310.Dockerfile -t marieai/marie:3.0.26-cuda --no-cache
+DOCKER_BUILDKIT=1 docker build . --cpuset-cpus="0-32"  --build-arg PIP_TAG="[standard]" -f ./Dockerfiles/gpu-310.Dockerfile -t marieai/marie:3.0.28-cuda --no-cache
