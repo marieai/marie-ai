d4421c27d27446b81c5d42dd1a16075a45ee5015
Grzegorz Bugaj <gregbugaj@yahoo.com>
2024-03-04 10:47:29 -0600

    fix: add support for extracting version numbers

--- scripts/build-container.sh
+++ scripts/build-container.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
-DOCKER_BUILDKIT=1 docker build . --cpuset-cpus="0-32"  --build-arg PIP_TAG="[standard]" -f ./Dockerfiles/gpu-310.Dockerfile -t marieai/marie:3.0.26-cuda --no-cache
+CPU_COUNT=$(grep -c ^processor /proc/cpuinfo)
+MARIE_VERSION=$(sed -n '/^__version__/p' ./marie/__init__.py | cut -d \' -f2)
+
+echo "Building Marie version: $MARIE_VERSION"
+echo "CPU count: $CPU_COUNT"
+
+DOCKER_BUILDKIT=1 docker build . --cpuset-cpus="0-$CPU_COUNT"  --build-arg PIP_TAG="[standard]" -f ./Dockerfiles/gpu-310.Dockerfile -t marieai/marie:$MARIE_VERSION-cuda --no-cache
