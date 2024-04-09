0b607727d3325e4d8089338fb7a0dcefe21d1a54
Grzegorz Bugaj <gregbugaj@yahoo.com>
2024-04-09 18:27:51 -0500

    feat: initial core for form registration

---
+++ marie/components/document_registration/base.py
@@ -0,0 +1,63 @@
+from abc import abstractmethod
+from typing import List, Optional
+
+from docarray import DocList
+
+from marie.api.docs import MarieDoc
+from marie.base_handler import BaseHandler
+from marie.logging.logger import MarieLogger
+
+
+class BaseDocumentBoundaryRegistration(BaseHandler):
+    def __init__(
+        self,
+        **kwargs,
+    ) -> None:
+        super().__init__()
+        self.logger = MarieLogger(self.__class__.__name__).logger
+
+    @abstractmethod
+    def predict(
+        self,
+        documents: DocList[MarieDoc],
+        words: Optional[List[List[str]]] = None,
+        boxes: Optional[List[List[List[int]]]] = None,
+        batch_size: Optional[int] = None,
+    ) -> DocList:
+        """
+        Predict document boundaries. This method must be implemented by subclasses.
+        :param documents:
+        :param words:
+        :param boxes:
+        :param batch_size:
+        """
+        pass
+
+    def run(
+        self,
+        documents: DocList,
+        words: Optional[List[List[str]]] = None,
+        boxes: Optional[List[List[List[int]]]] = None,
+        batch_size: Optional[int] = None,
+    ) -> DocList:
+        """
+        Run the document boundary registration on the given documents.
+
+        :param documents: the documents to find the registration for
+        :param words:
+        :param boxes:
+        :param batch_size:
+        :return:
+        """
+        if documents:
+            results = self.predict(
+                documents=documents, words=words, boxes=boxes, batch_size=batch_size
+            )
+        else:
+            results = DocList()
+
+        document_id = [document.id for document in documents]
+
+        # output = {"documents": results}
+        self.logger.info(f"Registered documents with IDs: {document_id}")
+        return results
