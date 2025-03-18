1ea54a53c3dfffa418489a7c398aa22c29deb598
Grzegorz Bugaj <gregbugaj@yahoo.com>
2025-03-18 07:04:21 -0500

    wip

--- workspaces/document-named-entity-recognition/qwen.py
+++ workspaces/document-named-entity-recognition/qwen.py
@@ -1,5 +1,8 @@
+import ast
 import io
+import json
 import os
+import random
 import subprocess
 import uuid
 from threading import Thread
@@ -7,8 +10,8 @@ from threading import Thread
 import gradio as gr
 import numpy as np
 import torch
-from PIL import Image
-from qwen_vl_utils import process_vision_info
+from PIL import Image, ImageColor, ImageDraw, ImageFont
+from qwen_vl_utils import process_vision_info, smart_resize
 from transformers import (
     AutoProcessor,
     BitsAndBytesConfig,
@@ -16,9 +19,11 @@ from transformers import (
     TextIteratorStreamer,
 )
 
-# Model and Processor Loading (Done once at startup)
+additional_colors = [
+    colorname for (colorname, colorcode) in ImageColor.colormap.items()
+]
+
 MODEL_ID = "Qwen/Qwen2-VL-7B-Instruct"
-# MODEL_ID = "Qwen/Qwen2-VL-2B-Instruct"
 MODEL_ID = "Qwen/Qwen2.5-VL-7B-Instruct"
 
 # Configure BitsAndBytesConfig for 8-bit precision
@@ -28,61 +33,29 @@ bnb_config = BitsAndBytesConfig(
 # bnb_config = None
 
 # https://huggingface.co/Qwen/Qwen2-VL-2B-Instruct/discussions/10
-min_pixels = 256 * 28 * 28
-max_pixels = 1200 * 28 * 28
-max_pixels = 1000 * 28 * 28
+min_pixels = 512 * 28 * 28
+max_pixels = 2048 * 28 * 28
 
-if False:
-    model = (
-        Qwen2VLForConditionalGeneration.from_pretrained(
-            MODEL_ID,
-            trust_remote_code=True,
-            torch_dtype=torch.float16,
-            attn_implementation="flash_attention_2",
-            quantization_config=bnb_config,
-        )
-        .to("cuda")
-        .eval()
-    )
+from transformers import (
+    AutoProcessor,
+    Qwen2_5_VLForConditionalGeneration,
+    TextIteratorStreamer,
+)
 
-    processor = AutoProcessor.from_pretrained(
-        MODEL_ID, min_pixels=min_pixels, max_pixels=max_pixels, trust_remote_code=True
-    )
+# Load model and processor
+# ckpt = "Qwen/Qwen2.5-VL-3B-Instruct"
+model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+    MODEL_ID,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    # quantization_config=bnb_config,
+    attn_implementation="flash_attention_2",
+    quantization_config=bnb_config,
+).to("cuda")
 
-if True:
-    from transformers import (
-        AutoProcessor,
-        Qwen2_5_VLForConditionalGeneration,
-        TextIteratorStreamer,
-    )
-
-    # Load model and processor
-    # ckpt = "Qwen/Qwen2.5-VL-3B-Instruct"
-    model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-        MODEL_ID,
-        torch_dtype=torch.bfloat16,
-        trust_remote_code=True,
-        quantization_config=bnb_config,
-    ).to("cuda")
-    processor = AutoProcessor.from_pretrained(
-        MODEL_ID, trust_remote_code=True, min_pixels=min_pixels, max_pixels=max_pixels
-    )
-
-if False:
-    from unsloth import FastVisionModel  # FastLanguageModel for LLMs
-
-    ckpt = "Qwen/Qwen2.5-VL-7B-Instruct"
-
-    model, tokenizer = FastVisionModel.from_pretrained(
-        "unsloth/Qwen2.5-VL-7B-Instruct",
-        load_in_4bit=True,  # Use 4bit to reduce memory use. False for 16bit LoRA.
-        use_gradient_checkpointing="unsloth",  # True or "unsloth" for long context
-    )
-
-    processor = AutoProcessor.from_pretrained(
-        ckpt, trust_remote_code=True, min_pixels=min_pixels, max_pixels=max_pixels
-    )
-
+processor = AutoProcessor.from_pretrained(
+    MODEL_ID, trust_remote_code=True, min_pixels=min_pixels, max_pixels=max_pixels
+)
 
 DESCRIPTION = f"[{MODEL_ID}]"
 
@@ -140,6 +113,104 @@ def process_file_upload(file_path):
     return None, None
 
 
+def parse_json(json_output):
+    # Parsing out the markdown fencing
+    lines = json_output.splitlines()
+    for i, line in enumerate(lines):
+        if line == "```json":
+            json_output = "\n".join(
+                lines[i + 1 :]
+            )  # Remove everything before "```json"
+            json_output = json_output.split("```")[
+                0
+            ]  # Remove everything after the closing "```"
+            break  # Exit the loop once "```json" is found
+    return json_output
+
+
+def plot_bounding_boxes(im, bounding_boxes, input_width, input_height):
+    """
+    Plots bounding boxes on an image with markers for each a name, using PIL, normalized coordinates, and different colors.
+
+    Args:
+        img_path: The path to the image file.
+        bounding_boxes: A list of bounding boxes containing the name of the object
+         and their positions in normalized [y1 x1 y2 x2] format.
+    """
+
+    # Load the image
+    img = im
+    width, height = img.size
+    print(img.size)
+    draw = ImageDraw.Draw(img)
+
+    # Define a list of colors
+    colors = [
+        'red',
+        'green',
+        'blue',
+        'yellow',
+        'orange',
+        'pink',
+        'purple',
+        'brown',
+        'gray',
+        'beige',
+        'turquoise',
+        'cyan',
+        'magenta',
+        'lime',
+        'navy',
+        'maroon',
+        'teal',
+        'olive',
+        'coral',
+        'lavender',
+        'violet',
+        'gold',
+        'silver',
+    ] + additional_colors
+
+    # Parsing out the markdown fencing
+    bounding_boxes = parse_json(bounding_boxes)
+    font = ImageFont.truetype("NotoSansCJK-Regular.ttc", size=14)
+    try:
+        json_output = ast.literal_eval(bounding_boxes)
+    except Exception as e:
+        end_idx = bounding_boxes.rfind('"}') + len('"}')
+        truncated_text = bounding_boxes[:end_idx] + "]"
+        json_output = ast.literal_eval(truncated_text)
+
+    # Iterate over the bounding boxes
+    for i, bounding_box in enumerate(json_output):
+        # Select a color from the list
+        color = colors[i % len(colors)]
+
+        # Convert normalized coordinates to absolute coordinates
+        abs_y1 = int(bounding_box["bbox_2d"][1] / input_height * height)
+        abs_x1 = int(bounding_box["bbox_2d"][0] / input_width * width)
+        abs_y2 = int(bounding_box["bbox_2d"][3] / input_height * height)
+        abs_x2 = int(bounding_box["bbox_2d"][2] / input_width * width)
+
+        if abs_x1 > abs_x2:
+            abs_x1, abs_x2 = abs_x2, abs_x1
+
+        if abs_y1 > abs_y2:
+            abs_y1, abs_y2 = abs_y2, abs_y1
+
+        # Draw the bounding box
+        draw.rectangle(((abs_x1, abs_y1), (abs_x2, abs_y2)), outline=color, width=4)
+
+        # Draw the text
+        if "label" in bounding_box:
+            draw.text(
+                (abs_x1 + 8, abs_y1 + 6), bounding_box["label"], fill=color, font=font
+            )
+
+    # Display the image
+    img.show()
+
+
 def qwen_inference(media_input, text_input=None):
     if isinstance(media_input, str):  # If it's a filepath
         media_path = media_input
@@ -157,6 +228,7 @@ def qwen_inference(media_input, text_input=None):
                     "Unsupported media type. Please upload an image or video."
                 )
     print(media_path)
+    image = Image.open(media_path)
 
     messages = [
         {
@@ -174,11 +246,11 @@ def qwen_inference(media_input, text_input=None):
     text = processor.apply_chat_template(
         messages, tokenize=False, add_generation_prompt=True
     )
-    image_inputs, video_inputs = process_vision_info(messages)
+    # image_inputs, video_inputs = process_vision_info(messages)
     inputs = processor(
         text=[text],
-        images=image_inputs,
-        videos=video_inputs,
+        images=[image],
+        videos=None,
         padding=True,
         return_tensors="pt",
     ).to("cuda")
@@ -201,6 +273,13 @@ def qwen_inference(media_input, text_input=None):
     for new_text in streamer:
         buffer += new_text
         yield buffer
+
+    print("done")
+    print(buffer)
+
+    json_output = parse_json(buffer)
+    print(json_output)
+    plot_bounding_boxes(image, buffer, input_width, input_height)
 
 
 css = """
@@ -233,3 +312,11 @@ with gr.Blocks(css=css) as demo:
         submit_btn.click(qwen_inference, [input_media, text_input], [output_text])
 
 demo.launch(debug=True)
+
+# https://github.com/QwenLM/Qwen2.5-VL/issues/721
+# https://github.com/QwenLM/Qwen2.5-VL/blob/main/cookbooks/spatial_understanding.ipynb
+# Outline the position of  each table column header and output all the coordinates in JSON format with table header text as label.
+
+# https://www.linkedin.com/pulse/advancing-document-ai-table-column-detection-made-simple-vaghela-px6ac
+# https://github.com/VikParuchuri/marker/tree/master/marker
+# python table_recognition.py --debug --images /home/greg/dev/rms/grapnel-g5/assets/TID-100985/ANNOTATED/OUT/tables/fragments/5_0-1200.png --skip_table_detection
