9435548d9c337e98ecbbfcb6e0681b34ac91688d
Grzegorz Bugaj <gregbugaj@yahoo.com>
2025-03-19 07:04:01 -0500

    feat: initial integration of table_rec

---
+++ marie/components/table_rec/loader.py
@@ -0,0 +1,71 @@
+from typing import Optional
+
+import torch
+
+from marie.common.load import ModelLoader
+from marie.components.table_rec.model.config import (
+    DonutSwinTableRecConfig,
+    SuryaTableRecConfig,
+    SuryaTableRecDecoderConfig,
+)
+from marie.components.table_rec.model.encoderdecoder import TableRecEncoderDecoderModel
+from marie.components.table_rec.processor import SuryaTableRecProcessor
+from marie.settings import settings
+
+
+class TableRecModelLoader(ModelLoader):
+    def __init__(self, checkpoint: Optional[str] = None):
+        super().__init__(checkpoint)
+
+        if self.checkpoint is None:
+            self.checkpoint = settings.TABLE_REC_MODEL_CHECKPOINT
+
+    def model(
+        self, device=settings.TORCH_DEVICE_MODEL, dtype=settings.MODEL_DTYPE
+    ) -> TableRecEncoderDecoderModel:
+        if device is None:
+            device = settings.TORCH_DEVICE_MODEL
+        if dtype is None:
+            dtype = settings.MODEL_DTYPE
+
+        config = SuryaTableRecConfig.from_pretrained(self.checkpoint)
+        decoder_config = config.decoder
+        decoder = SuryaTableRecDecoderConfig(**decoder_config)
+        config.decoder = decoder
+
+        encoder_config = config.encoder
+        encoder = DonutSwinTableRecConfig(**encoder_config)
+        config.encoder = encoder
+
+        model = TableRecEncoderDecoderModel.from_pretrained(
+            self.checkpoint, config=config, torch_dtype=dtype
+        )
+
+        model = model.to(device)
+        model = model.eval()
+
+        if settings.COMPILE_ALL or settings.COMPILE_TABLE_REC:
+            torch.set_float32_matmul_precision('high')
+            torch._dynamo.config.cache_size_limit = 16
+            torch._dynamo.config.suppress_errors = False
+
+            print(
+                f"Compiling table recognition model {self.checkpoint} on device {device} with dtype {dtype}"
+            )
+            compile_args = {'backend': 'openxla'} if device == 'xla' else {}
+            model.encoder = torch.compile(model.encoder, **compile_args)
+            model.decoder = torch.compile(model.decoder, **compile_args)
+
+        print(
+            f"Loaded table recognition model {self.checkpoint} on device {device} with dtype {dtype}"
+        )
+        return model
+
+    def processor(self) -> SuryaTableRecProcessor:
+        processor = SuryaTableRecProcessor(self.checkpoint)
+
+        processor.token_pad_id = 0
+        processor.token_eos_id = 1
+        processor.token_bos_id = 1
+        processor.token_query_end_id = 4
+        return processor
---
+++ tests/integration/table_rec/conftest.py
@@ -0,0 +1,35 @@
+import os
+
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+
+import pytest
+from PIL import Image, ImageDraw
+
+from marie.components.table_rec import TableRecPredictor
+
+
+@pytest.fixture(scope="session")
+def table_rec_predictor() -> TableRecPredictor:
+    table_rec_predictor = TableRecPredictor()
+    yield table_rec_predictor
+    del table_rec_predictor
+
+
+@pytest.fixture()
+def test_image():
+    image = Image.new("RGB", (1024, 1024), "white")
+    draw = ImageDraw.Draw(image)
+    draw.text((10, 10), "Hello World", fill="black", font_size=72)
+    draw.text((10, 200), "This is a sentence of text.\nNow it is a paragraph.\nA three-line one.", fill="black",
+              font_size=24)
+    return image
+
+
+@pytest.fixture()
+def test_image_tall():
+    image = Image.new("RGB", (4096, 4096), "white")
+    draw = ImageDraw.Draw(image)
+    draw.text((10, 10), "Hello World", fill="black", font_size=72)
+    draw.text((4000, 4000), "This is a sentence of text.\n\nNow it is a paragraph.\n\nA three-line one.", fill="black",
+              font_size=24)
+    return image
