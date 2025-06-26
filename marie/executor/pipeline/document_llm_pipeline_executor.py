61648f728af26be5d2668dc0db078039780e5f6f
rsteele <rsteele@rmsweb.com>
2025-06-26 18:00:13 -0500

    feat: Add `merge/metadata` endpoint to pipeline executors
    * refactor parameter parsing
    * replace error returns with error raises to ensure the node fails if passed illegal parameters

--- marie/executor/pipeline/document_llm_pipeline_executor.py
+++ marie/executor/pipeline/document_llm_pipeline_executor.py
@@ -1,16 +1,15 @@
-from typing import List, Optional, Union
+from typing import List, Optional
 
-import torch
 from docarray import DocList
 
 from marie import requests
 from marie.api import AssetKeyDoc
-from marie.executor.pipeline.document_pipeline_executor import DocumentPipelineExecutor
+from marie.executor.pipeline.document_pipeline_executor import PipelineExecutor
 from marie.logging_core.predefined import default_logger as logger
 from marie.pipe.llm_pipeline import LLMPipeline
 
 
-class DocumentLLMPipelineExecutor(DocumentPipelineExecutor):
+class DocumentLLMPipelineExecutor(PipelineExecutor):
     """Executor for pipeline document proccessing"""
 
     def __init__(
@@ -20,13 +19,11 @@ class DocumentLLMPipelineExecutor(DocumentPipelineExecutor):
         num_worker_preprocess: int = 4,
         storage: dict[str, any] = None,
         pipelines: List[dict[str, any]] = None,
-        dtype: Optional[Union[str, torch.dtype]] = None,
         **kwargs,
     ):
-        super().__init__(
-            name, device, num_worker_preprocess, storage, pipelines, dtype, **kwargs
-        )
+        super().__init__(name, device, num_worker_preprocess, storage, **kwargs)
         logger.info(f"Starting Pipeline Setup")
+        logger.info(f"Pipelines config: {pipelines}")
         has_cuda = True if self.device.type.startswith("cuda") else False
         self.pipeline = LLMPipeline(pipelines_config=pipelines, cuda=has_cuda)
 
@@ -34,10 +31,10 @@ class DocumentLLMPipelineExecutor(DocumentPipelineExecutor):
     def handle_classify(
         self, docs: DocList[AssetKeyDoc], parameters: dict, *args, **kwargs
     ):
-        return self.run_pipeline(docs, parameters, *args, **kwargs)
+        return self.run_pipeline(self.pipeline, docs, parameters, *args, **kwargs)
 
     @requests(on="/document/index")
     def handle_index(
         self, docs: DocList[AssetKeyDoc], parameters: dict, *args, **kwargs
     ):
-        return self.run_pipeline(docs, parameters, *args, **kwargs)
+        return self.run_pipeline(self.pipeline, docs, parameters, *args, **kwargs)
