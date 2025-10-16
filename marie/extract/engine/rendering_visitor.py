206b80ea96d281653cc0e0964a662e13bbd6fe85
rsteele <rsteele@rmsweb.com>
2025-10-16 15:21:08 -0500

    feat: doc level field mapping
    * Consolidate Field conversion mapping
    * fix: Empty sections do not make empty remit elements

--- marie/extract/engine/rendering_visitor.py
+++ marie/extract/engine/rendering_visitor.py
@@ -8,7 +8,12 @@ from xml.etree.ElementTree import tostring
 
 from marie.extract.engine.base import BaseProcessingVisitor
 from marie.extract.models.exec_context import ExecutionContext
-from marie.extract.models.match import MatchSection, MatchSectionType, SubzeroResult
+from marie.extract.models.match import (
+    Field,
+    MatchSection,
+    MatchSectionType,
+    SubzeroResult,
+)
 from marie.extract.results.annotation_highlighter import AnnotationHighlighter
 from marie.extract.structures import UnstructuredDocument
 from marie.logging_core.logger import MarieLogger
@@ -71,6 +76,12 @@ class MatchSectionRenderingVisitor(BaseProcessingVisitor):
         """
         Recursively process sections and their fields to generate XML structure
         """
+        # FIXME: this is a hack and should be externalized
+        document_level_field_names = {
+            "CHECK_NUMBER",
+            "CHECK_AMOUNT",
+        }
+
         # Use BFS to process all sections
         queue = deque([parent])
 
@@ -84,42 +95,16 @@ class MatchSectionRenderingVisitor(BaseProcessingVisitor):
                 queue.extend(current.sections)
                 continue
             # Create a remit element for this section
-            remit = ET.SubElement(root_element, "remits")
+            remit = ET.Element("remits")
 
-            # Add all fields from this section
+            # Add all fields from this section to their respective elements
             for field in current.matched_non_repeating_fields:
-                field_elem = ET.SubElement(remit, "fields")
+                field_elem = self._convert_field(field)
 
-                ET.SubElement(field_elem, "fieldName").text = field.field_name or ""
-                ET.SubElement(field_elem, "fieldType").text = (
-                    field.field_type or "ALPHA"
-                )
-                ET.SubElement(field_elem, "isRequired").text = str(
-                    field.is_required
-                ).lower()
-                ET.SubElement(field_elem, "value").text = field.value or ""
-                ET.SubElement(field_elem, "x").text = str(field.x)
-                ET.SubElement(field_elem, "y").text = str(field.y)
-                ET.SubElement(field_elem, "width").text = str(field.width)
-                ET.SubElement(field_elem, "height").text = str(field.height)
-                ET.SubElement(field_elem, "page").text = str(field.page)
-                ET.SubElement(field_elem, "xdpi").text = str(field.xdpi)
-                ET.SubElement(field_elem, "ydpi").text = str(field.ydpi)
-                ET.SubElement(field_elem, "confidence").text = str(field.confidence)
-                ET.SubElement(field_elem, "scrubbed").text = str(field.scrubbed).lower()
-
-                if field.uuid:
-                    ET.SubElement(field_elem, "uuid").text = field.uuid
-
-                if field.reference_uuid:
-                    ET.SubElement(field_elem, "referenceUuid").text = (
-                        field.reference_uuid
-                    )
-
-                if field.value_original is not None:
-                    ET.SubElement(field_elem, "valueOriginal").text = (
-                        field.value_original
-                    )
+                if field.field_name in document_level_field_names:
+                    root_element.append(field_elem)
+                else:
+                    remit.append(field_elem)
 
             # Process all matched_field_rows for this section
             if current.matched_field_rows is not None:
@@ -129,49 +114,85 @@ class MatchSectionRenderingVisitor(BaseProcessingVisitor):
                     )  # Directly create <serviceLines> for each row
                     # our structure is foobared as there is no container for the fields  but rather they are added one at a  time
                     for field in row.fields:
-                        field_elem = ET.SubElement(service_lines, "fields")
-
-                        ET.SubElement(field_elem, "fieldName").text = (
-                            field.field_name or ""
-                        )
-                        ET.SubElement(field_elem, "fieldType").text = (
-                            field.field_type or "ALPHA"
-                        )
-                        ET.SubElement(field_elem, "isRequired").text = str(
-                            field.is_required
-                        ).lower()
-                        ET.SubElement(field_elem, "value").text = field.value or ""
-                        ET.SubElement(field_elem, "x").text = str(field.x)
-                        ET.SubElement(field_elem, "y").text = str(field.y)
-                        ET.SubElement(field_elem, "width").text = str(field.width)
-                        ET.SubElement(field_elem, "height").text = str(field.height)
-
-                        # Add optional field attributes
-                        date_format = ET.SubElement(field_elem, "dateFormat")
-                        ET.SubElement(date_format, "formatString").text = (
-                            field.date_format or ""
+                        self._convert_field(
+                            field,
+                            parent_element=service_lines,
+                            default_date_format="",
+                            default_column_name="",
                         )
 
-                        ET.SubElement(field_elem, "columnName").text = (
-                            field.column_name or ""
-                        )
-                        ET.SubElement(field_elem, "page").text = str(field.page)
-                        ET.SubElement(field_elem, "xdpi").text = str(field.xdpi)
-                        ET.SubElement(field_elem, "ydpi").text = str(field.ydpi)
-                        ET.SubElement(field_elem, "confidence").text = str(
-                            field.confidence
-                        )
-                        ET.SubElement(field_elem, "scrubbed").text = str(
-                            field.scrubbed
-                        ).lower()
-
-                        if field.value_original is not None:
-                            ET.SubElement(field_elem, "valueOriginal").text = (
-                                field.value_original
-                            )
+            # Only add the remit element if it has sub elements
+            if len(remit) > 0:
+                root_element.append(remit)
 
             # Add child sections to the queue
             queue.extend(current.sections)
+
+    def _convert_field(
+        self,
+        field: Field,
+        parent_element: ET.Element = None,
+        default_date_format: str = None,
+        default_column_name: str = None,
+    ) -> ET.Element:
+        """
+        Converts a Field object into its XML representation.
+
+        This method generates an XML element that represents a given `Field` object. The generated
+        element contains all relevant attributes of the `Field` object, including its name, type,
+        position, and other metadata. Optional attributes such as `date_format`, `column_name`,
+        `uuid`, and `reference_uuid` are also added when available. The newly created XML element
+        can optionally be appended to a parent XML element.
+
+        Parameters:
+            field (Field): The Field object to be converted into an XML element.
+            parent_element (ET.Element, optional): An optional parent XML element to which the
+                generated XML element will be appended. Defaults to None.
+            default_date_format (str, optional): An optional default date format to use if the
+                `Field` object does not have a specific `date_format`. Defaults to None.
+            default_column_name (str, optional): An optional default column name to use if the
+                `Field` object does not have a specific `column_name`. Defaults to None.
+
+        Returns:
+            ET.Element: The XML representation of the Field object.
+        """
+        field_elem = ET.Element("fields")
+        if parent_element is not None:
+            parent_element.append(field_elem)
+
+        ET.SubElement(field_elem, "fieldName").text = field.field_name or ""
+        ET.SubElement(field_elem, "fieldType").text = field.field_type or "ALPHA"
+        ET.SubElement(field_elem, "isRequired").text = str(field.is_required).lower()
+        ET.SubElement(field_elem, "value").text = field.value or ""
+        ET.SubElement(field_elem, "x").text = str(field.x)
+        ET.SubElement(field_elem, "y").text = str(field.y)
+        ET.SubElement(field_elem, "width").text = str(field.width)
+        ET.SubElement(field_elem, "height").text = str(field.height)
+        ET.SubElement(field_elem, "page").text = str(field.page)
+        ET.SubElement(field_elem, "xdpi").text = str(field.xdpi)
+        ET.SubElement(field_elem, "ydpi").text = str(field.ydpi)
+        ET.SubElement(field_elem, "confidence").text = str(field.confidence)
+        ET.SubElement(field_elem, "scrubbed").text = str(field.scrubbed).lower()
+
+        # Add optional field attributes
+        if field.date_format or default_date_format is not None:
+            date_format = ET.SubElement(field_elem, "dateFormat")
+            ET.SubElement(date_format, "formatString").text = (
+                field.date_format or default_date_format
+            )
+        if field.column_name or default_column_name is not None:
+            ET.SubElement(field_elem, "columnName").text = (
+                field.column_name or default_column_name
+            )
+        if field.uuid:
+            ET.SubElement(field_elem, "uuid").text = field.uuid
+        if field.reference_uuid:
+            ET.SubElement(field_elem, "referenceUuid").text = field.reference_uuid
+
+        if field.value_original is not None:
+            ET.SubElement(field_elem, "valueOriginal").text = field.value_original
+
+        return field_elem
 
     def _pretty_print_xml(self, element: ET.Element) -> str:
         """
@@ -199,7 +220,6 @@ class MatchSectionRenderingVisitor(BaseProcessingVisitor):
 
 
 def highlight_tables(doc: UnstructuredDocument, frames: list, output_dir: str):
-
     def tables_only(ann, _line) -> bool:
         try:
             return ann.annotation_type == "TABLE"
